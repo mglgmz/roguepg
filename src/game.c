3171e1c4b9025d3c62cf8f6ce03d7e7f0bfca358
Miguel Gomez Bistiancic <miguel.gomez.bistiancic@gmail.com>
2024-01-30 22:10:35 -0300

    Better iteration, started enemy movement

--- src/game.c
+++ src/game.c
@@ -107,7 +107,10 @@ static Vector2 gridOrigin = { screenWidth / 2 - TW / 2, screenHeight / 2 - TW / 
 // V2
 #define DEFAULT_DUNGEON_SIZE 50
 
-#define ROLL_MAX 1000
+#define ROLL_MAX 1000          
+
+#define BLOCK_ID 0
+#define EMPTY_ID 1
 
 #define BLOCK_CHANCE 130
 #define ENEMY_CHANCE 25
@@ -121,23 +124,49 @@ static Vector2 gridOrigin = { screenWidth / 2 - TW / 2, screenHeight / 2 - TW / 
 Vector2 dungeonOrigin = { 0 };
 int layout[DEFAULT_DUNGEON_SIZE][DEFAULT_DUNGEON_SIZE];
 
+#define MAX_ENEMIES 50
+
 int enemiesLeft = 0;
 int tresuresLeft = 0;
+
+// Enemies
+typedef struct Enemy {
+    int type;
+    Vector2 position;
+    int detection;
+    int actionsPerTurn;
+} Enemy;
+Enemy enemies[MAX_ENEMIES];
+
 
 static void GenerateDungeon(void) {
     dungeonOrigin = (Vector2) { -DEFAULT_DUNGEON_SIZE * TW / 2, -DEFAULT_DUNGEON_SIZE * TW / 2 };
     Rectangle initialBounds = (Rectangle){ 0, 0, DEFAULT_DUNGEON_SIZE, DEFAULT_DUNGEON_SIZE };
     for(int x = 0; x < DEFAULT_DUNGEON_SIZE; x++) {
         for(int y = 0; y < DEFAULT_DUNGEON_SIZE; y++) {
+
+            layout[x][y] = EMPTY_ID;
+
             if((playerDest.x == x && playerDest.y == y)) continue;
+            
             int roll = GetRandomValue(0, ROLL_MAX);
-            layout[x][y] = 0;
-            if(roll < BLOCK_CHANCE) layout[x][y] = 1;
+            
+            if(roll < BLOCK_CHANCE) layout[x][y] = BLOCK_ID;
             else {
                 roll = GetRandomValue(0, ROLL_MAX);
-                if (roll < ENEMY_CHANCE) { 
-                    layout[x][y] = ENEMY_ID_START + GetRandomValue(0, NUM_ENEMIES);
+                if (enemiesLeft < MAX_ENEMIES && roll < ENEMY_CHANCE) { 
+                    // layout[DEFAULT_DUNGEON_SIZE - x][DEFAULT_DUNGEON_SIZE - y] = ENEMY_ID_START + GetRandomValue(0, NUM_ENEMIES);
+                    Enemy enemy = {
+                        .type = ENEMY_ID_START + GetRandomValue(0, NUM_ENEMIES),
+                        .actionsPerTurn = 1
+                    };
+                    enemy.position = (Vector2) { x, y };
+                    enemy.detection = GetRandomValue(5, 15);
+                    enemies[enemiesLeft] = enemy;
                     enemiesLeft++;
+
+                    // TODO: Generate one random enemy, save it's details
+
                 } else {
                     roll = GetRandomValue(0, ROLL_MAX);
                     if (roll < TRESURE_CHANCE) {
@@ -148,6 +177,10 @@ static void GenerateDungeon(void) {
             } 
         }
     }
+}
+
+static Vector2 CoordsToPos(int x, int y, int tw) {
+    return (Vector2){ dungeonOrigin.x + x * tw, dungeonOrigin.y + y * tw };
 }
 
 static void DrawDungeon(void) {
@@ -167,11 +200,11 @@ static void DrawDungeon(void) {
 
     for(int x = 0; x < DEFAULT_DUNGEON_SIZE; x++) {
         for(int y = 0; y < DEFAULT_DUNGEON_SIZE; y++) {
-            if(layout[x][y] == 1) {
+            if(layout[x][y] == BLOCK_ID) {
                 DrawRectangle(dungeonOrigin.x + x * TW, dungeonOrigin.y + y * TW, TW, TW, DARKGRAY);
                 DrawRectangleLines(dungeonOrigin.x + x * TW, dungeonOrigin.y + y * TW, TW, TW, LIGHTGRAY);
             }else if(layout[x][y] >= ENEMY_ID_START && layout[x][y] <= ENEMY_ID_START + NUM_ENEMIES) {
-                DrawRectangle(dungeonOrigin.x + x * TW, dungeonOrigin.y + y * TW, TW, TW, RED);
+                //DrawRectangle(dungeonOrigin.x + x * TW, dungeonOrigin.y + y * TW, TW, TW, RED);
             }
             else if(layout[x][y] == TRESURE_ID) {
                 DrawRectangle(dungeonOrigin.x + x * TW, dungeonOrigin.y + y * TW, TW, TW, GOLD);
@@ -184,16 +217,24 @@ static void DrawDungeon(void) {
         }
     }
 
+    for(int i=0; i< MAX_ENEMIES; i++) {
+        Enemy* enemy = &enemies[i];
+        Vector2 pos = CoordsToPos(enemy->position.x, enemy->position.y, TW);
+        DrawRectangle(pos.x, pos.y, TW, TW, RED);
+    }
+
 }
 
 
-static Vector2 CoordsToPos(int x, int y, int tw) {
-    return (Vector2){ dungeonOrigin.x + x * tw, dungeonOrigin.y + y * tw };
+
+
+static int GetValue(int x, int y) {
+    if ( x < 0 || x >= DEFAULT_DUNGEON_SIZE || y < 0 || y >= DEFAULT_DUNGEON_SIZE) return 0;
+    return layout[x][y];
 }
 
 static int CanMove(int x, int y, int xOff, int yOff) {
-    return x + xOff >= 0 && y+yOff >=0 && x + xOff <  DEFAULT_DUNGEON_SIZE && y + yOff < DEFAULT_DUNGEON_SIZE
-        && layout[x+xOff][y+yOff] == 0;
+    return GetValue(x + xOff, y + yOff); 
 }
 
 ///////////////////////////////////////////////////
@@ -209,6 +250,25 @@ static void DungeonTurn(void) {
     currentTurn = DUNGEON_TURN;
     dungeonRemaingTime = maxDungeonTime;
     //TODO change time and just change turn when dungeon has no remaing actions
+
+    for(int i=0; i< MAX_ENEMIES; i++) {
+        Enemy* enemy = &enemies[i];
+        
+        // TODO check if enemy detected player
+
+        int dx = playerDest.x < enemy->position.x ? -1 :
+                playerDest.x > enemy->position.x ? 1 : 0;
+        // TODO Account for number of actions
+
+        if(CanMove(enemy->position.x, enemy->position.y, dx, 0)) {
+            enemy->position = (Vector2) { enemy->position.x + dx, enemy->position.y };
+        } else {
+            int dy = playerDest.y < enemy->position.y ? -1 :
+                playerDest.y > enemy->position.y ? 1 : 0;
+            if(CanMove(enemy->position.x, enemy->position.y, dx, 0))
+                enemy->position = (Vector2) { enemy->position.x, enemy->position.y + dy };
+        }
+    }
 }
 
 static void UpdateMap(void) {
@@ -280,6 +340,7 @@ static void RenderMap(void) {
             
         EndMode2D();
 
+        DrawText(TextFormat("Enemies Left: %i", enemiesLeft), 5, gameHeight - 30, 10, COLOR_3);
         DrawText(TextFormat("CurrentTurn: %s", currentTurn == PLAYER_TURN ? "Player" : "Dungeon"), 5, gameHeight - 15, 10, COLOR_3);
         // DrawText(TextFormat("TotalRooms: %i", totalRooms), 5, gameHeight - 30, 10, COLOR_3);
 
